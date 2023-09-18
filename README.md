92c41a3c947bac064b732a23a9049ab5809911c9
Faolain <Faolain@users.noreply.github.com>
2023-09-17 21:07:14 -0400

    fix: deployment race condition

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-env:
-  ENV_NAME: pr-${{ github.event.pull_request.number }}-${{ github.sha }}
+# Railway PR Deploy Action
+
+## Introduction to Railway
+[Railway](https://railway.app/) is a PaaS Cloud Service which makes it easy to deploy simple or complex services which can depend on many other services (APIs Databases etc)
+
+This action makes it simple to deploy a PR environment based on an already existing environment in order to be able to test ephemeral API environments.
+
+## Inputs
+
+| Name                |         Required         | Default | Description                                                                                       |
+|---------------------|:------------------------:|---------|---------------------------------------------------------------------------------------------------|
+| RAILWAY_API_TOKEN        | [x] |         | Railway Token. See: https://railway.app/account/tokens                                       |
+| PROJECT_ID      |  [x]    |  |The id of the project to create environments on. Can be found on Settings -> General page                               |
+| SRC_ENVIRONMENT_NAME        | [x]|         | The name of the environment to base the PRs off of.                                 |
+| SRC_ENVIRONMENT_ID   | [ ]|         | The id of the environment to base the PRs off of. If this is provided, SRC_ENVIRONMENT_NAME will be ignored. '                  |
+| DEST_ENV_NAME       | [x] |         | The name of the deployed PR environment. Usually a combination of pr-<PR_NUMBER>-<SHORT_COMMIT_HASH> passed inside of workflow                 |
+| ENV_VARS         | [ ]  |         | The environment variables to set on the PR environment. Should be a JSON object of key value pairs. e.g. ` '{"database_url": "${{ env.DYNAMIC_VAR }}", "other_key": "other_value"}'`                                            |
+| PROVIDER   |  [x]  |         | 'The provider to use for the PR environment. Can be python, see https://nixpacks.com/docs/guides/configuring-builds for more info.'                                                                 |
+| branch_name               | [x]  |         | The name of the branch you will be deploying from Github. Should be `${{ github.head_ref }}`         
+| DEPLOYMENT_MAX_TIMEOUT       |  [ ]|         | 'The maximum amount of time to wait for the deployment to finish. Defaults to 10 minutes.'
+
+## Outputs
+
+### `domain`
+
+The url of PR deployment preview. This would be an api endpoint for example for the service deployed.
+
+## How To Use
+
+```
+    - name: Create PR environment on Railway
+      if: github.event.action == 'opened'
+      uses: Faolain/railway-pr-deploy@v1.0.5
+      with:
+        RAILWAY_API_TOKEN: ${{ secrets.RAILWAY_TOKEN }}
+        PROJECT_ID: ${{ secrets.RAILWAY_PROJECT_ID }}
+        SRC_ENVIRONMENT_NAME: production
+        DEST_ENV_NAME: pr-${{ github.event.pull_request.number }}
+        PROVIDER: python
+        ENV_VARS: '{"database_url": "${{ env.DYNAMIC_VAR }}", "other_key": "other_value"}'
+        branch_name: ${{ github.head_ref }}
+``````
--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const axios = require('axios');
 const core = require('@actions/core');
 const { request, gql, GraphQLClient } = require('graphql-request')
 
@@ -9,12 +8,13 @@ const SRC_ENVIRONMENT_NAME = core.getInput('SRC_ENVIRONMENT_NAME');
 const SRC_ENVIRONMENT_ID = core.getInput('SRC_ENVIRONMENT_ID');
 const DEST_ENV_NAME = core.getInput('DEST_ENV_NAME');
 const ENV_VARS = core.getInput('ENV_VARS');
-const PROVIDER = core.getInput('PROVIDER');
 const ENDPOINT = 'https://backboard.railway.app/graphql/v2';
 
 // Github Required Inputs
-const BRANCH_NAME = core.getInput('branch_name');
-const REPOSITORY = core.getInput('repository');
+const BRANCH_NAME = core.getInput('branch_name') || "feat-railway-7";
+
+// Optional Inputs
+const DEPLOYMENT_MAX_TIMEOUT = core.getInput('MAX_TIMEOUT');
 
 async function railwayGraphQLRequest(query, variables) {
     const client = new GraphQLClient(ENDPOINT, {
@@ -29,7 +29,7 @@ async function railwayGraphQLRequest(query, variables) {
     }
 }
 
-async function getEnvironmentId() {
+async function getEnvironments() {
     let query =
         `query environments($projectId: String!) {
             environments(projectId: $projectId) {
@@ -37,9 +37,18 @@ async function getEnvironmentId() {
                     node {
                         id
                         name
+                        deployments {
+                            edges {
+                                node {
+                                    id
+                                    status
+                                }
+                            }
+                        }
                         serviceInstances {
                             edges {
                                 node {
+                                    id
                                     domains {
                                         serviceDomains {
                                             domain
@@ -63,11 +72,13 @@ async function getEnvironmentId() {
 }
 
 async function createEnvironment(sourceEnvironmentId) {
+    console.log("Creating Environment... based on source environment ID:", sourceEnvironmentId)
     try {
         let query = gql`
         mutation environmentCreate($input: EnvironmentCreateInput!) {
             environmentCreate(input: $input) {
                 id
+                name
                 createdAt
                 deploymentTriggers {
                     edges {
@@ -89,6 +100,7 @@ async function createEnvironment(sourceEnvironmentId) {
                                     id
                                 }
                             }
+                            serviceId
                         }
                     }
                 }
@@ -134,6 +146,7 @@ async function updateEnvironment(environmentId, serviceId, variables) {
 }
 
 async function deploymentTriggerUpdate(deploymentTriggerId) {
+    console.log("Updating Deploying Trigger to new Branch Name")
     try {
         let query = gql`
         mutation deploymentTriggerUpdate($id: String!, $input: DeploymentTriggerUpdateInput!) {
@@ -156,22 +169,59 @@ async function deploymentTriggerUpdate(deploymentTriggerId) {
     }
 }
 
-async function checkIfEnvironmentExists() {
-    let response = await getEnvironmentId();
-    const filteredEdges = response.environments.edges.filter((edge) => edge.node.name === DEST_ENV_NAME);
-    return filteredEdges.length == 1 ? { environmentId: filteredEdges[0].node.id, serviceId: filteredEdges[0].serviceInstances.edges[0].serviceId } : null;
+async function monitorDeploymentStatus() {
+    const parsedTimeout = parseInt(DEPLOYMENT_MAX_TIMEOUT);
+    const MAX_TIMEOUT = isNaN(parsedTimeout) ? 600000 : parsedTimeout;
+
+    const startTime = Date.now();
+
+    while (true) { // Infinite loop, but we have break and throw conditions inside.
+        if (Date.now() - startTime > MAX_TIMEOUT) {
+            throw new Error('Maximum timeout duration reached trying to monitor deployment. Exiting.');
+        }
+
+        // Get Environments to check if the deployment has finished
+        let response = await getEnvironments();
+
+        const filteredEdges = response?.environments?.edges?.filter((edge) => edge.node.name === DEST_ENV_NAME);
+
+        if (!filteredEdges || filteredEdges.length === 0) {
+            throw new Error('Unexpected response structure or environment not found. Exiting.');
+        }
+
+        let deploymentStatus = filteredEdges[0]?.node?.deployments?.edges?.[0]?.node?.status;
+
+        if (!deploymentStatus) {
+            throw new Error('Deployment status not found in the response. Exiting.');
+        }
+
+        if (deploymentStatus === 'SUCCESS') {
+            break;  // Exit the loop
+        } else if (deploymentStatus === 'FAILED') {
+            throw new Error('Deployment failed. Please check the Railway Dashboard for more information.');
+        } else if (['BUILDING', 'DEPLOYING', 'INITIALIZING', 'QUEUED', 'WAITING'].includes(deploymentStatus)) {
+            console.log('Deployment is still in progress. Status:', deploymentStatus, '. Waiting 20 seconds and trying again...');
+            await new Promise(resolve => setTimeout(resolve, 20000)); // Wait for 20 seconds and try again
+        } else {
+            throw new Error(`Unhandled deployment status. Please check the Railway Dashboard for more information. Status: ${deploymentStatus}. Response: ${JSON.stringify(response)}`);
+        }
+    }
 }
 
-async function deleteEnvironment(environmentId) {
+async function serviceInstanceRedeploy(environmentId, serviceId) {
+    console.log("Redeploying Service...")
+    console.log("Environment ID:", environmentId)
+    console.log("Service ID:", serviceId)
     try {
         let query = gql`
-        mutation environmentDelete($id: String!) {
-            environmentDelete(id: $id)
+        mutation serviceInstanceRedeploy($environmentId: String!, $serviceId: String!) {
+            serviceInstanceRedeploy(environmentId: $environmentId, serviceId: $serviceId)
         }
         `
 
         let variables = {
             "environmentId": environmentId,
+            "serviceId": serviceId
         }
 
         return await railwayGraphQLRequest(query, variables)
@@ -182,29 +232,31 @@ async function deleteEnvironment(environmentId) {
 
 async function run() {
     try {
-        const environmentIfExists = await checkIfEnvironmentExists();
-        if (environmentIfExists) {
+        // Get Environments to check if the environment already exists
+        let response = await getEnvironments();
+
+        // Filter the response to only include the environment name we are looking to create
+        const filteredEdges = response.environments.edges.filter((edge) => edge.node.name === DEST_ENV_NAME);
+
+        // If there is a match this means the environment already exists
+        if (filteredEdges.length == 1) {
             throw new Error('Environment already exists. Please delete the environment via API or Railway Dashboard and try again.')
         }
 
-        console.log('Environment does not exist, creating new Environment...')
         let srcEnvironmentId = SRC_ENVIRONMENT_ID;
 
-        // Get Source Environment ID to base new PR environment from
+        // If no source ENV_ID provided get Source Environment ID to base new PR environment from (aka use the same environment variables)
         if (!SRC_ENVIRONMENT_ID) {
-            let response = await getEnvironmentId();
             srcEnvironmentId = response.environments.edges.filter((edge) => edge.node.name === SRC_ENVIRONMENT_NAME)[0].node.id;
         }
 
         // Create the new Environment based on the Source Environment
         const createdEnvironment = await createEnvironment(srcEnvironmentId);
+        console.log("Created Environment:")
         console.dir(createdEnvironment, { depth: null })
 
         const { id: environmentId } = createdEnvironment.environmentCreate;
         const { id: deploymentTriggerId } = createdEnvironment.environmentCreate.deploymentTriggers.edges[0].node;
-
-        // Set the Deployment Trigger Branch
-        await deploymentTriggerUpdate(deploymentTriggerId);
 
         // Get the Service ID
         const { serviceId } = createdEnvironment.environmentCreate.serviceInstances.edges[0].node;
@@ -212,8 +264,21 @@ async function run() {
         // Update the Environment Variables
         const updatedEnvironmentVariables = await updateEnvironment(environmentId, serviceId, ENV_VARS);
 
+        // Wait for the created environment to finish initializing
+        console.log("Waiting 15 seconds for deployment to initialize and become available")
+        await new Promise(resolve => setTimeout(resolve, 15000)); // Wait for 15 seconds
+
+        // Wait for the initial deployment (which is autocreated by createEnvironment) to finish, otherwise you cannot run concurrent deployments
+        await monitorDeploymentStatus();
+
+        // Set the Deployment Trigger Branch
+        await deploymentTriggerUpdate(deploymentTriggerId);
+
+        // Redeploy the Service
+        await serviceInstanceRedeploy(environmentId, serviceId);
+
         const { domain } = createdEnvironment.environmentCreate.serviceInstances.edges[0].node.domains.serviceDomains[0];
-        console.log('domain', domain)
+        console.log('Domain:', domain)
         core.setOutput('service_domain', domain);
     } catch (error) {
         console.error('Error in API calls:', error);
